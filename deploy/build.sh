4d8ccd58ae56c24c3a2f0491711e378c04532802
tnatsvli <tnatsvli@mail.uoguelph.ca>
2018-01-15 23:09:01 -0500

    Update dockerfiles

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,19 +4,16 @@ host=192.168.0.11
 # host=localhost
 
 echo "Building images"
-docker build -q -t skivrige/nginx nginx
-docker build -q -t skivrige/scraper ../scraper
-docker build -q -t skivrige/backend ../backend
-docker build -q -t skivrige/frontend ../frontend
+docker build -q -t skivrige.writing/nginx nginx
+docker build -q -t skivrige.writing/backend ../backend
+docker build -q -t skivrige.writing/frontend ../frontend
 
 echo "Tagging images"
-docker tag skivrige/nginx $host:5000/skivrige/nginx
-docker tag skivrige/scraper $host:5000/skivrige/scraper
-docker tag skivrige/backend $host:5000/skivrige/backend
-docker tag skivrige/frontend $host:5000/skivrige/frontend
+docker tag skivrige.writing/nginx $host:5000/skivrige.writing/nginx
+docker tag skivrige.writing/backend $host:5000/skivrige.writing/backend
+docker tag skivrige.writing/frontend $host:5000/skivrige.writing/frontend
 
 echo "Pushing images"
-docker push $host:5000/skivrige/nginx
-docker push $host:5000/skivrige/scraper
-docker push $host:5000/skivrige/backend
-docker push $host:5000/skivrige/frontend
+docker push $host:5000/skivrige.writing/nginx
+docker push $host:5000/skivrige.writing/backend
+docker push $host:5000/skivrige.writing/frontend
